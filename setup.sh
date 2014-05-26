eb53c274e542395e8cffc6b628edbf384975bd3c
Jake Tobak <jtobak@gmail.com>
2014-05-26 13:44:43 +0000

    fixinf setup.sh some more...

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh -o -f ${HOME}/bin/zsh ]; then
     git clone http://github.com/JakeTobak/dotfiles.git
   fi
   # Set the default shell to zsh if it isn't currently set to zsh
-  if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+  if [ ! $(echo $SHELL) == $(which zsh) ]; then
     chsh -s $(which zsh)
   fi
 else
